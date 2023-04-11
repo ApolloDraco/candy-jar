37f9a793eac722c0bbdaa1e80081cc0443a9b129
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:27:58 -0400

    Update CandyJar.java

--- CandyJar.java
+++ CandyJar.java
@@ -3,18 +3,32 @@
 */
 
 import java.util.Scanner;
-public class CandyJar{    
-     public static void main(String []args){
-       int n = 10, k = 5;
-       int num;
-       Scanner sc = new Scanner(System.in);
-       num = sc.nextInt();
-       if(num >= 1 && num <= 5) {
-          System.out.println("Number of candies sold: " + num);
-          System.out.print("Number of candies left: " + (n - num));
-       } else {
-          System.out.println("Invalid input");
-          System.out.print("Number of candies left: " + n);
-}
-}
+
+public class CandyJar {
+    // Define constants
+    private static final int TOTAL_CANDIES = 10;
+    private static final int MAX_SOLD_CANDIES = 5;
+    
+    public static void main(String[] args) {
+        int candiesLeft = TOTAL_CANDIES;
+        int numSold;
+        Scanner sc = new Scanner(System.in);
+        
+        // Prompt user to enter number of candies sold
+        System.out.print("Enter number of candies sold: ");
+        numSold = sc.nextInt();
+        
+        // Check if input is valid
+        if (numSold >= 1 && numSold <= MAX_SOLD_CANDIES) {
+            System.out.println("Number of candies sold: " + numSold);
+            candiesLeft = TOTAL_CANDIES - numSold;
+            System.out.println("Number of candies left: " + candiesLeft);
+        } else {
+            // Display error message and prompt user to try again
+            System.out.println("Invalid input. Please enter a value between 1 and " + MAX_SOLD_CANDIES + ".");
+            System.out.println("Number of candies left: " + candiesLeft);
+        }
+        
+        sc.close();
+    }
 }
